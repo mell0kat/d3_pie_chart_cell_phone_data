1af924bdad0d3ab5dd476d38395ebb3aa523820a
Katherine Mello <katherine.mello8@gmail.com>
2018-02-02 15:16:03 -0500

    Uses cellphone usage data for 3 months to construct pie chart

--- js/main.js
+++ js/main.js
@@ -1,13 +1,65 @@
 const svg = d3.select('svg')
+const body = d3.select('body')
 const width = +svg.attr('width')
 const height = +svg.attr('height')
 const radius = Math.min(width, height) / 2
-const g = svg.append('g').attr('transform', `translate(${width / 2},${height / 2})`)
-const color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+
+const data = {
+	'October': [
+		{ member: 'Robby', usage: 3.2 },
+		{ member: 'Billy', usage: 4.4 },
+		{ member: 'Betsy', usage: 4.4 },
+		{ member: 'Katherine', usage: 0.9 },
+		{ member: 'Willy', usage: 0.1 },
+		{ member: 'Pam', usage: 1.0 },
+	],
+	'November': [
+		{ member: 'Robby', usage: 1.7 },
+		{ member: 'Billy', usage: 3.6 },
+		{ member: 'Betsy', usage: 5.3 },
+		{ member: 'Katherine', usage: 1.4 },
+		{ member: 'Willy', usage: 0.0 },
+		{ member: 'Pam', usage: 1.3 },
+	],
+	'December': [
+		{ member: 'Robby', usage: 1.4 },
+		{ member: 'Billy', usage: 2.6 },
+		{ member: 'Betsy', usage: 6.3 },
+		{ member: 'Katherine', usage: 0.9 },
+		{ member: 'Willy', usage: 0.1 },
+		{ member: 'Pam', usage: 2.4 },
+	]
+}
+
+const colorMap = {
+	October: ["#fff0f6", "#520339"],
+	November: ["#fcffe6", "#254000"],
+	December: ["#e6f7ff", "#002766"],
+}
+
+const onchange = () => {
+	const selectValue = d3.select('select')
+		.property('value')
+	renderPie(selectValue)
+}
+
+const select = d3.select('select')
+	.attr('class', 'select')
+	.on('change', onchange)
+
+const options = select
+	.selectAll('option')
+	.data(d3.keys(data))
+	.enter()
+	.append('option')
+	.text(d => d)
+
+console.log('options', options)
+
 
 const pie = d3.pie()
 	.sort(null)
-	.value(d => d.population)
+	.value(d => d.usage)
 
 
 const path = d3.arc()
@@ -18,30 +70,36 @@ const label = d3.arc()
 	.outerRadius(radius - 40)
 	.innerRadius(radius - 40)
 
-const data = [
-{ age: '<5', population: 2704659 },
-{ age: '5-13', population: 4499890 },
-{ age: '14-17', population: 2159981 },
-{ age: '18-24', population: 3853788 },
-{ age: '25-44', population: 14106543 },
-{ age: '45-64', population: 8819342 },
-{ age: '≥65', population: 612463 },
-]
 
 
-const arc = g.selectAll('.arc')
-.data(pie(data))
-.enter()
-.append('g')
-.attr('class', 'arc')
-
-arc.append('path')
-	.attr('d', path)
-	.attr('fill', d => color(d.data.age))
-
-arc.append('text')
-	.attr("transform", d => "translate(" + label.centroid(d) + ")")
-  .attr("dy", "0.35em")
-	.text(function(d) { return d.data.age; });
 
 
+
+const renderPie = (month) => {
+
+	const g = svg.append('g').attr('transform', `translate(${width / 2},${height / 2})`)
+
+	const color = d3.scaleLinear()
+		.domain([0, d3.max(data[month], d => d.usage) + .25*(d3.max(data[month], d => d.usage))])
+		.interpolate(d3.interpolateRgb)
+		.range(colorMap[month])
+
+	const arc = g.selectAll('.arc')
+		.data(pie(data[month]))
+		.enter()
+		.filter(d => d.data.usage > 0.05)
+		.append('g')
+		.attr('class', 'arc')
+
+	arc.append('path')
+		.attr('d', path)
+		.attr('fill', d => color(d.data.usage))
+
+	arc.append('text')
+		.attr("transform", d => "translate(" + label.centroid(d) + ")")
+	  .attr("dy", "0.35em")
+		.text(function(d) { return d.data.member; });
+
+}
+
+renderPie(d3.keys(data)[0])
